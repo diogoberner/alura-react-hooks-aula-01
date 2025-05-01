63d073843ee3682489ce42c671d34b705423ad2e
Diogo CS Berner <diogoberner@gmail.com>
2025-05-01 15:00:39 -0300

    Refactor Cronometro component to utilize TimerContext for state management and remove local state handling

--- src/components/Cronometro/index.jsx
+++ src/components/Cronometro/index.jsx
@@ -4,64 +4,11 @@ import BotoesModos from "./BotoesModos";
 import Timer from "./Timer";
 import SwitchMusica from "./SwitchMusica";
 import BotaoCronometro from "./BotaoCronometro";
-import { useContext, useEffect, useRef } from "react";
+import { useContext } from "react";
 import { TimerContext } from "../../context/TimerContext";
 
 export default function Cronometro() {
-  const musicaRef = useRef(null);
-
-  const {
-    modoAtual,
-    setModoAtual,
-    tempoRestante,
-    setTempoRestante,
-    emExecucao,
-    setEmExecucao,
-    modos,
-  } = useContext(TimerContext);
-
-  useEffect(() => {
-    let intervalo;
-
-    if (emExecucao && tempoRestante > 0) {
-      intervalo = setInterval(() => {
-        setTempoRestante((prev) => prev - 1);
-      }, 1000);
-    } else if (tempoRestante === 0) {
-      setModoAtual((prev) => {
-        const modoAtualIndex = modos.findIndex((modo) => modo.id === prev.id);
-        const proximoModo = modos[(modoAtualIndex + 1) % modos.length];
-        setTempoRestante(proximoModo.duracao);
-        return proximoModo;
-      });
-      setEmExecucao(false);
-    }
-
-    return () => clearInterval(intervalo);
-  }, [emExecucao, tempoRestante, modoAtual]);
-
-  const controlarMusica = () => {
-    if (musicaRef.current) {
-      musicaRef.current.play();
-    }
-  };
-
-  const alternarCronometro = () => {
-    setEmExecucao((prev) => !prev);
-
-    if (!emExecucao) {
-      controlarMusica();
-    } else {
-      musicaRef.current.pause();
-    }
-  };
-
-  const selecionarModo = (novoModo) => {
-    setModoAtual(novoModo);
-    setTempoRestante(novoModo.duracao);
-    setEmExecucao(false);
-    musicaRef.current.pause();
-  };
+  const { selecionarModo, alternarCronometro, musicaRef } = useContext(TimerContext);
 
   return (
     <div className={styles["cronometer"]}>
--- src/context/TimerContext/index.jsx
+++ src/context/TimerContext/index.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { createContext } from "react";
 
 export const TimerContext = createContext(null);
 
 export const TimerProvider = ({ children }) => {
+  const musicaRef = useRef(null);
   const [modos, setModos] = useState([
     {
       id: "foco",
@@ -25,6 +26,49 @@ export const TimerProvider = ({ children }) => {
   const [tempoRestante, setTempoRestante] = useState(modoAtual.duracao);
   const [emExecucao, setEmExecucao] = useState(false); // Para saber se o cronômetro está rodando
 
+  useEffect(() => {
+    let intervalo;
+
+    if (emExecucao && tempoRestante > 0) {
+      intervalo = setInterval(() => {
+        setTempoRestante((prev) => prev - 1);
+      }, 1000);
+    } else if (tempoRestante === 0) {
+      setModoAtual((prev) => {
+        const modoAtualIndex = modos.findIndex((modo) => modo.id === prev.id);
+        const proximoModo = modos[(modoAtualIndex + 1) % modos.length];
+        setTempoRestante(proximoModo.duracao);
+        return proximoModo;
+      });
+      setEmExecucao(false);
+    }
+
+    return () => clearInterval(intervalo);
+  }, [emExecucao, tempoRestante, modoAtual]);
+
+  const controlarMusica = () => {
+    if (musicaRef.current) {
+      musicaRef.current.play();
+    }
+  };
+
+  const alternarCronometro = () => {
+    setEmExecucao((prev) => !prev);
+
+    if (!emExecucao) {
+      controlarMusica();
+    } else {
+      musicaRef.current.pause();
+    }
+  };
+
+  const selecionarModo = (novoModo) => {
+    setModoAtual(novoModo);
+    setTempoRestante(novoModo.duracao);
+    setEmExecucao(false);
+    musicaRef.current.pause();
+  };
+
   return (
     <TimerContext.Provider
       value={{
@@ -36,6 +80,9 @@ export const TimerProvider = ({ children }) => {
         setEmExecucao,
         modos,
         setModos,
+        alternarCronometro,
+        selecionarModo,
+        musicaRef,
       }}
     >
       {children}
